bcde79a725977406fa750818843f36706234f9e1
LMoon <jjay033033@gmail.com>
2017-05-11 18:52:56 +0800

    1

---
+++ src/top/lmoon/constants/SystemConstants.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package top.lmoon.constants;
+
+/**
+ * @author guozy
+ * @date 2017-5-11
+ * 
+ */
+public class SystemConstants {
+	
+	public static final String LINE_BREAK = "\r\n";
+	
+	public static class Symbol{
+		
+		public static final String SPLIT = "_&&_";
+		
+		public static final String MSG = "msg";
+		
+		public static final String ADD = "cmdAdd";
+		
+		public static final String RED = "cmdRed";
+		
+		public static final String ONLINE = "online";
+		
+		public static final String OFFLINE = "offline";
+
+		public static final String SEND = "send";
+		
+		public static final String NOTICE = "notice";
+		
+	}
+
+}
---
+++ src/top/lmoon/util/Message.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package top.lmoon.util;
+
+/**
+ * @author guozy
+ * @date 2017-5-11
+ * 
+ */
+public class Message {
+	
+	private String cmd;
+	
+	
+	public Message(String msg){
+		
+	}
+	
+	
+
+}
